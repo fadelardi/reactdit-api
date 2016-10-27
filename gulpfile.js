ec28c953f741a7b372fbbb30ab78392ebada1455
fadelardi <fmadelardi@gmail.com>
2016-10-27 15:57:32 +0200

    Adding nodemon gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('nodemon', function() {
+	nodemon({});
+});
 
 gulp.task('lint', function() {
   return gulp.src(['**/*.js', '!node/modules/**'])
@@ -8,7 +13,7 @@ gulp.task('lint', function() {
 });
 
 
-gulp.task('default', ['lint'], function() {
+gulp.task('default', ['lint', 'nodemon'], function() {
   var watcher = gulp.watch('**/*.js', ['lint']);
   watcher.on('change', function(event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks... ');
